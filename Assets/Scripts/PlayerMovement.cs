3d897c155a3eb833bb653af9234ad6bcdb3f123a
Zachary Garza <ztg29@nau.edu>
2026-07-22 23:50:12 -0700

    player prefab original

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private CharacterController controller;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
